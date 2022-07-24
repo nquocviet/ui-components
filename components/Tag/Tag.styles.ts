c658c12563cf09abb0245327d9ab8f5a2d84729a
VietNguyen27 <nquocviet2307@gmail.com>
2022-07-24 16:11:14 +0700

    Tag component

---
+++ components/Tag/Tag.styles.ts
@@ -0,0 +1,33 @@
+export const styles = {
+  base: 'inline-flex items-center rounded-md overflow-hidden text-gray-700 font-medium ring-1 ring-gray-300',
+  sizes: {
+    sm: 'text-xs px-2 py-[3px] gap-[3px]',
+    md: 'text-sm px-[9px] py-0.5 gap-[5px]',
+    lg: 'text-sm px-2.5 py-1 gap-1.5',
+  },
+  leading: {
+    sm: '-ml-1',
+    md: '-ml-[5px]',
+    lg: '-ml-1.5',
+  },
+  trailing: {
+    sm: '-mr-1',
+    md: '-mr-[5px]',
+    lg: '-mr-1.5',
+  },
+  checkboxSizes: {
+    sm: 'xs',
+    md: 'sm',
+    lg: 'sm',
+  },
+  iconSizes: {
+    sm: 10,
+    md: 12,
+    lg: 14,
+  },
+  countSizes: {
+    sm: 'text-xs px-1',
+    md: 'text-xs px-[5px]',
+    lg: 'text-sm px-1.5',
+  },
+}
